f2822b6fcc88986fc7a0ac8e48c7e8ff0468fe0e
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-31 17:09:39 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,9 @@
-# code here!
+class School
+  def initialize (model)
+    @model= model
+  end
+  def
+  school = school.new("Bayside_High_School")
+  Bayside_High_School= school.new("Bayside_High_School")
+end
+    
