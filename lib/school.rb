c791ad0e40db678fbd7920ed28cfdd507ab0c7f9
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-31 17:40:42 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
-# code here!
+class School
+  def initialize (name)
+    @name= name
+    @roster = {}
+  end
+  def roster
+    @roster
+  end
+  
+  def add_student(name,grade)
+    @student_name = name
+    @grade = grade
+    if @roster[grade] ==true
+      @roster[grade]<< student_name
+      @roster = { 9 =>[AC Slatter]
+    end
+    
+    def add_student (student_name, grade)
+      if @roster [grade]
+        @roster[grade] << student_name
+      else
+        @roster[grade] = []
+        @roster[grade] << student_name
+      end
+      
+      def initialize(school_name)
+        @school_name = school_name
+        @roster = Hash.new {|h,k| h[k] = []}
+      end
+      
+    end
+    
+    
+  end
+  
+  def
+  
+    
